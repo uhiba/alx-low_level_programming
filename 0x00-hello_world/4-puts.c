72a5b7fcce7bfaf90429fe24a2b72b3ee69dcef7
ouahiba <ouahibastaf@gmail.com>
2023-02-17 14:59:57 -0800

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+
 /**
- * main - A program that prints a line with puts function 
- * Return: 0 (Success)
- */
+*main - A program that prints a line with puts function 
+*return: 0 (Success)
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
