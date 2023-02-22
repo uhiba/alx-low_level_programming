b0cf0d5d7b8637188ee55d8d9fd2b0cc44ccb819
ouahiba <ouahibastaf@gmail.com>
2023-02-21 16:47:48 -0800

    Functions, nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - adds two integres and returns the result
+ * @a: int to added to b
+ * @b: int to added to a
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+int c;
+c = a + b;
+return (c);
+}
