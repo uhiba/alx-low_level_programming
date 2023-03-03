1576b9cdf12512762d30779bbe301c739e0d0a5d
ouahiba <ouahibastaf@gmail.com>
2023-03-03 12:15:25 -0800

    100-root

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rot13 - encorder rot13
+ * @s: pointer to string parameters
+ * Return: *s
+ */
+char *rot13(char *s)
+{
+int i;
+int j;
+char datal[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; j < 52; j++)
+{
+if (s[i] == datal[j])
+{
+s[i] = datarot[j];
+break;
+}
+}
+}
+return (s);
+}
