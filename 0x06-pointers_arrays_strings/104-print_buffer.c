7cf6d1ced017c181e5c71176a33ce177f0243241
ouahiba <ouahibastaf@gmail.com>
2023-03-03 17:40:07 -0800

    ./104-buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -19,13 +19,13 @@ return;
 while (o < size)
 {
 j = size - o < 10 ? size - o : 10;
-printf("%08x:", o);
+printf("%08x: ", o);
 for (i = 0; i < 10; i++)
 {
 if (i < j)
 printf("%02x", *(b + o + i));
 else
-printf(" ");
+printf("  ");
 if (i % 2)
 {
 printf(" ");
